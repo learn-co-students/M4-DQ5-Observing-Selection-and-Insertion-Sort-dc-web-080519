86f02d5f42e253f750f50863617cf080a071f90c
icydwei <robpaik@gmail.com>
2019-10-14 11:02:56 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,15 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+  let el = arr[i];
+  let j;
+
+  for (j = i - 1; j >= 0 && arr[j] > el; j--) {
+    arr[j + 1] = arr[j];
+  }
+  arr[j + 1] = el;
+  }
+  return arr;
 }
